9861cec8a105c4357ba4d72906ac3ce38f316f16
Evan Anderson <evananderson@thelinuxman.us>
2015-09-21 11:26:00 -0400

    Initial commit
    Signed-off-by: Evan Anderson <evananderson@thelinuxman.us>

---
+++ src/us/thelinuxman/sortcompare/Main.java
@@ -0,0 +1,135 @@
+package us.thelinuxman.sortcompare;
+
+/**
+ *  Author: Evan Anderson
+ *  Date: 9/20/2015
+ *  Description: Main class to analyze sorting algorithm runtime
+ */
+public class Main {
+
+    private static final int MAX_DATA_VALUE = 100000;
+    private static final int DEFAULT_BUCKET_SIZE = 5;
+
+    public static void main(String[] args) {
+        if (args.length != 2) { // catch invalid args
+            printHelp();
+            System.exit(1);
+        }
+
+        //parse sort and data type
+        String sortType = args[0].toUpperCase().substring(0, 1);
+        String dataType = args[0].toUpperCase().substring(1);
+        String sortLongName = "unknown";
+
+        int length = Integer.parseInt(args[1]);
+        long startTime; //start time in ms
+        long runtime = 0; //runtime in ms
+
+        int[] data = generateArray(dataType, length);
+
+        if (data[0] == -1) { //catch invalid array type
+            printHelp(); //provide guidance
+            System.exit(1);
+        }
+
+        System.out.println("Sort start...");
+
+        switch (sortType) {
+            case "C": //counting sort
+                sortLongName = "Counting sort";
+                startTime = System.currentTimeMillis();
+                data = Sorts.countingSort(data, MAX_DATA_VALUE);
+                runtime = System.currentTimeMillis() - startTime;
+                break;
+            case "R": //radix sort
+                sortLongName = "Radix sort";
+                int max = findRange(data)[1];
+                startTime = System.currentTimeMillis();
+                data = Sorts.radixSort(data, max); //TODO: implement, fix digit values
+                runtime = System.currentTimeMillis() - startTime;
+                break;
+            case "B": //bucket sort
+                sortLongName = "Bucket sort";
+                int[] range = findRange(data);
+                startTime = System.currentTimeMillis();
+                data = Sorts.bucketSort(data, range[1], range[0], DEFAULT_BUCKET_SIZE);
+                runtime = System.currentTimeMillis() - startTime;
+                break;
+            case "I":
+                sortLongName = "Insertion sort";
+                startTime = System.currentTimeMillis();
+                Sorts.insertionSort(data);
+                runtime = System.currentTimeMillis() - startTime;
+                break;
+            case "Q":
+                sortLongName = "Quick sort";
+                startTime = System.currentTimeMillis();
+                Sorts.quickSort(data);
+                runtime = System.currentTimeMillis() - startTime;
+                break;
+            default:
+
+        }
+
+        //printSorted(data);
+        if (sortLongName.equals("unknown")) {
+            System.out.println("Invalid sort choice");
+        } else {
+            System.out.println(sortLongName + " finished in " + runtime / 1000 + " seconds (" + runtime + " ms).");
+        }
+    }
+
+    private static int[] generateArray(String type, int length) {
+        int[] data = new int[length];
+        switch (type) {
+            case "R":
+                for (int i = 0; i < length; i++) {
+                    data[i] = (int) (Math.random() * MAX_DATA_VALUE + 1);
+                }
+                break;
+            case "S":
+                for (int i = 0; i < length; i++) {
+                    data[i] = i % MAX_DATA_VALUE;
+                }
+                break;
+            case "E":
+                for (int i = length - 1; i > 0; i--) {
+                    data[i] = i % MAX_DATA_VALUE;
+                }
+                break;
+            default:
+                System.out.println("Invalid data array type");
+                data[0] = -1;
+                return data;
+        }
+        return data;
+    }
+
+    private static int[] findRange(int[] array) {
+        int range[] = {array[0], array[0]};
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < range[0]) {
+                range[0] = array[i];
+            } else if (array[i] > range[1]) {
+                range[1] = array[i];
+            }
+        }
+        return range;
+    }
+
+    private static void printSorted(int[] array) {
+        for (int value: array) {
+            System.out.print(Integer.toString(value) + " ");
+        }
+        System.out.println();
+    }
+
+    private static void printHelp() {
+        System.out.print("Usage information\n\nArguments:\n\tXY #\n\t\tX: valid sort code\n\t\tY: valid data " +
+                "code\n\t\t#: array length to generate\n");
+        System.out.print("Sorts:\n\t\tI - Insertion sort [O(n)]\n\t\tQ - Quick sort [O(n lg n)]\n\t\t" +
+                "C - Counting sort [O(n)]\n\t\tR - Radix sort [O(n)]\n\t\tB - Bucket sort [O(n)]\n");
+        System.out.print("Data:\n\t\tR: random data\n\t\tS: sorted data\n\t\tE: reverse sorted data\n");
+    }
+
+}
---
+++ src/us/thelinuxman/sortcompare/Sorts.java
@@ -0,0 +1,138 @@
+package us.thelinuxman.sortcompare;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *  Author: Evan Anderson
+ *  Date: 9/20/2015
+ *  Description: Implementation of several sorting algorithms
+ */
+public class Sorts {
+    protected static int[] countingSort(int[] array, int max) {
+        int[] sorted = new int[array.length];
+        int[] working = new int[max + 1];
+        for (int i = 0; i < max; i++) {
+            working[i] = 0;
+        }
+        for (int i = 0; i < array.length; i++) {
+            working[array[i]] += 1;
+        }
+        for (int i = 1; i <= max; i++) {
+            working[i] += working[i - 1];
+        }
+        for (int i = array.length - 1; i >= 0; i--) {
+            sorted[working[array[i]] - 1] = array[i];
+            working[array[i]]--;
+        }
+        return sorted;
+    }
+
+    protected static int[] radixSort(int[] array, int max) {
+        int place = 1;
+        while(max / place > 0) {
+            int[] sorted = new int[array.length];
+            int[] working = new int[10];
+            for (int i = 0; i < array.length; i++) {
+                working[getDigit(array[i], place)] += 1;
+            }
+            for (int i = 1; i < working.length; i++) {
+                working[i] += working[i - 1];
+            }
+            for (int i = array.length - 1; i >= 0; i--) {
+                int digit = getDigit(array[i], place);
+                sorted[working[digit] - 1] = array[i];
+                working[digit]--;
+            }
+            array = sorted;
+            place *= 10;
+        }
+        return array;
+    }
+
+    private static int getDigit(int num, int place) {
+        return (num / place) % 10;
+    }
+
+    protected static int[] bucketSort(int[] array, int max, int min, int bucketSize) {
+        int length = array.length;
+        List<List<Integer>> buckets = new ArrayList<>();
+        int bucketCount = (max - min) / bucketSize + 1;
+        for (int i = 0; i < bucketCount; i++) {
+            buckets.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < length; i++) {
+            buckets.get((array[i] - min) / bucketSize).add(array[i]);
+        }
+
+        int arrayIndex = 0;
+        for (int i = 0; i < buckets.size(); i++) {
+            Integer[] sorted = buckets.get(i).toArray(new Integer[buckets.get(i).size()]);
+            sorted = insertionSort(sorted);
+            for (int j = 0; j < sorted.length; j++) {
+                array[arrayIndex++] = sorted[j];
+            }
+        }
+
+        return array;
+    }
+
+    //O(n^2) sort
+
+    protected static int[] insertionSort(int[] array) {
+        Integer[] newArray = new Integer[array.length];
+        for (int i = 0; i < array.length; i++) {
+            newArray[i] = array[i];
+        }
+        insertionSort(newArray);
+        int[] primitiveArray = new int[newArray.length];
+        for (int i = 0; i < newArray.length; i++) {
+            primitiveArray[i] = newArray[i];
+        }
+        return primitiveArray;
+    }
+
+    protected static Integer[] insertionSort(Integer[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int key = array[i];
+            int j = i - 1;
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = key;
+        }
+        return array;
+    }
+
+    //O(n lg n) sort
+    protected static void quickSort(int[] array) {
+        quickSort(array, 0, array.length - 1);
+    }
+
+    protected static void quickSort(int[] array, int p, int r) {
+        if (p > r) return;
+        int pivot = partition(array, p, r);
+        quickSort(array, p, pivot - 1); //sort upper half
+        quickSort(array, pivot + 1, r); //sort lower half
+    }
+
+    private static int partition(int[] array, int p, int r) {
+        int end = array[r];
+        int i = p - 1;
+        for (int j = p; j < r - 1; j++) {
+            if (array[j] <= end) {
+                i = i + 1;
+                int tmp = array[i];
+                array[i] = array[j];
+                array[j] = tmp;
+            }
+        }
+        int tmp = array[i + 1];
+        array[i + 1] = array[r];
+        array[r] = tmp;
+        return i + 1;
+    }
+
+}
